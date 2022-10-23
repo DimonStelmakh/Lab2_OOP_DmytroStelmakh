29e2cc17e0f60d1daea5267436ab83d10a09f284
DimonStelmakh <dmytro2004yur@gmail.com>
2022-10-24 01:07:49 +0300

    Дописана до кінця 2-га таска, ймовірно, недосконало, потребує рефакторингу і додаткового огляду

--- lab2p1task2.py
+++ lab2p1task2.py
@@ -1,9 +1,8 @@
-import re
-
 class Pizza:
-    def __init__(self, name, size):
+    def __init__(self, name, size, extra_ingredients=None):
         self.name = name
         self.size = size
+        self.extra_ingredients = extra_ingredients
 
     @property
     def name(self):
@@ -13,35 +12,51 @@ class Pizza:
     def size(self):
         return self.__size
 
+    @property
+    def extra_ingredients(self):
+        return self.__extra_ingredients
+
     @name.setter
     def name(self, value):
         if isinstance(value, str):
             self.__name = value
         else:
-            raise TypeError('TypeError. Expected "str" type')
+            raise TypeError('Expected "str" type')
 
     @size.setter
     def size(self, value):
         if isinstance(value, (int, float)):
             self.__size = value
         else:
-            raise TypeError('TypeError. Expected "int" or "float" type')
+            raise TypeError('Expected "int" or "float" type')
+
+    @extra_ingredients.setter
+    def extra_ingredients(self, value):
+        if value is None:
+            self.__extra_ingredients = None
+        elif isinstance(value, (str, list)):
+            self.__extra_ingredients = value
+        else:
+            raise TypeError('Expected "str" or "list" type')
 
     def __str__(self):
-        return f'{self.__name} pizza in size of {self.__size} centimeters'
+        if self.__extra_ingredients is None:
+            return f'{self.__name} pizza in size of {self.__size} centimeters'
+        else:
+            return f'{self.__name} pizza with extra {self.__extra_ingredients} in size of {self.__size} centimeters'
 
 
 class PizzaOfTheDay(Pizza):
-    def __init__(self, day_of_the_week, size):
+    def __init__(self, day_of_the_week, size, extra_ingredients=None):
         self.__dotw = self.dotw_interpreter(day_of_the_week)
         match self.__dotw:
-            case 1: super().__init__('Cheese', size)
-            case 2: super().__init__('Veggie', size)
-            case 3: super().__init__('Pepperoni', size)
-            case 4: super().__init__('Meat', size)
-            case 5: super().__init__('Margherita', size)
-            case 6: super().__init__('BBQ Chicken', size)
-            case 7: super().__init__('Supreme', size)
+            case 1: super().__init__('Cheese', size, extra_ingredients)
+            case 2: super().__init__('Veggie', size, extra_ingredients)
+            case 3: super().__init__('Pepperoni', size, extra_ingredients)
+            case 4: super().__init__('Meat', size, extra_ingredients)
+            case 5: super().__init__('Margherita', size, extra_ingredients)
+            case 6: super().__init__('BBQ Chicken', size, extra_ingredients)
+            case 7: super().__init__('Supreme', size, extra_ingredients)
             case 0: raise ValueError('This is not a day of the week!')
 
     @staticmethod
@@ -64,22 +79,31 @@ class PizzaOfTheDay(Pizza):
             else:
                 return 0
         elif isinstance(dotw, int):
-            if dotw == 7:  # тут заюзати регекс
+            if 1 <= dotw <= 7:
                 return dotw
             else:
-                return ValueError('ValueError. Numbers from 1 to 7 expected')
+                raise ValueError('Numbers from 1 to 7 expected')
         else:
-            raise TypeError('TypeError. "str" or "int"/"float" type expected')
+            raise TypeError('"str" or "int"/"float" type expected')
 
 
 def main():
     pizza1 = PizzaOfTheDay('5', 50)
-    print('An order is:')
+    print('An order №1 is:')
     print(pizza1)
 
     pizza2 = PizzaOfTheDay(7, 40)
-    print('An order is:')
+    print('An order №2 is:')
     print(pizza2)
+
+    pizza3 = PizzaOfTheDay('tue', 35, 'mushrooms')
+    print('An order №3 is:')
+    print(pizza3)
+
+    pizza4 = PizzaOfTheDay('Wednesday', 40, 'pineapple')
+    print('An order №4 is:')
+    print(pizza4)
 
 
 main()
+
