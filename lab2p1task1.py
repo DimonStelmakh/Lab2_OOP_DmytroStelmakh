ca1b80a7ec0be7fbbbf3bf5c2e53ad4651b7a2d3
DimonStelmakh <dmytro2004yur@gmail.com>
2022-10-25 10:01:47 +0300

    Дописано виведення типу квитка

--- lab2p1task1.py
+++ lab2p1task1.py
@@ -1,8 +1,9 @@
 class Ticket:
-    def __init__(self, number, price, regular_price):
+    def __init__(self, number, price, regular_price, ticket_type):
         self.number = number
         self.price = price
         self.regular_price = regular_price
+        self.ticket_type = ticket_type
 
     @property
     def number(self):
@@ -15,6 +16,10 @@ class Ticket:
     @property
     def regular_price(self):
         return int(self.__regular_price)
+
+    @property
+    def ticket_type(self):
+        return self.__ticket_type
 
     @number.setter
     def number(self, value):
@@ -37,28 +42,35 @@ class Ticket:
         else:
             raise TypeError('TypeError. Expected "int" or "float" type')
 
+    @ticket_type.setter
+    def ticket_type(self, value):
+        if isinstance(value, str):
+            self.__ticket_type = value
+        else:
+            raise TypeError('TypeError. Expected "str" type')
+
     def __str__(self):
-        return f'Number: {self.__number}, price: {int(self.__price)}'
+        return f'Number: {self.__number}, price: {int(self.__price)}, type: {self.__ticket_type}'
 
 
 class RegularTicket(Ticket):
     def __init__(self, number, regular_price=100):
-        super().__init__(number, regular_price, regular_price)
+        super().__init__(number, regular_price, regular_price, 'Regular')
 
 
 class AdvanceTicket(Ticket):
     def __init__(self, number, regular_price=100):
-        super().__init__(number, regular_price/10*6, regular_price)
+        super().__init__(number, regular_price/10*6, regular_price, 'Advance')
 
 
 class StudentTicket(Ticket):
     def __init__(self, number, regular_price=100):
-        super().__init__(number, regular_price/10*5, regular_price)
+        super().__init__(number, regular_price/10*5, regular_price, 'Student')
 
 
 class LateTicket(Ticket):
     def __init__(self, number, regular_price=100):
-        super().__init__(number, regular_price/10*11, regular_price)
+        super().__init__(number, regular_price/10*11, regular_price, 'Late')
 
 
 def main():
