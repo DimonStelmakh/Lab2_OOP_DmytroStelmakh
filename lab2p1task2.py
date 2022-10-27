cc93bc2b578362e3a64e0ff0c58bcc0ac64cb8e1
DimonStelmakh <dmytro2004yur@gmail.com>
2022-10-27 18:19:24 +0300

    Впроваджено систему інпутів

--- lab2p1task2.py
+++ lab2p1task2.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Pizza:
     def __init__(self, name, size, extra_ingredients=None):
         self.name = name
@@ -25,10 +28,10 @@ class Pizza:
 
     @size.setter
     def size(self, value):
-        if isinstance(value, (int, float)):
-            self.__size = value
-        else:
-            raise TypeError('Expected "int" or "float" type')
+        try:
+            self.__size = int(value)
+        except TypeError:
+            print('Enter a number!')
 
     @extra_ingredients.setter
     def extra_ingredients(self, value):
@@ -48,8 +51,8 @@ class Pizza:
 
 class PizzaOfTheDay(Pizza):
     def __init__(self, day_of_the_week, size, extra_ingredients=None):
-        self.__dotw = self.dotw_interpreter(day_of_the_week)
-        match self.__dotw:
+        # self.__dotw = self.dotw_interpreter(day_of_the_week)
+        match day_of_the_week:
             case 1: super().__init__('Cheese', size, extra_ingredients)
             case 2: super().__init__('Veggie', size, extra_ingredients)
             case 3: super().__init__('Pepperoni', size, extra_ingredients)
@@ -59,42 +62,38 @@ class PizzaOfTheDay(Pizza):
             case 7: super().__init__('Supreme', size, extra_ingredients)
             case 0: raise ValueError('This is not a day of the week!')
 
-    @staticmethod
-    def dotw_interpreter(dotw):
-        if dotw in ("Monday", "monday", "Mon", "mon", "First", "first", "1"):
-            return 1
-        elif dotw in ("Tuesday", "tuesday", "Tue", "tue", "2"):
-            return 2
-        elif dotw in ("Wednesday", "wednesday", "Wed", "wed", "3"):
-            return 3
-        elif dotw in ("Thursday", "thursday", "Thu", "thu", "4"):
-            return 4
-        elif dotw in ("Friday", "firday", "Fri", "fri", "5"):
-            return 5
-        elif dotw in ("Saturday", "saturday", "Sat", "sat", "6"):
-            return 6
-        elif dotw in ("Sunday", "sunday", "Sun", "sun", "Last", "last", "7"):
-            return 7
-        else:
-            return 0
+    # @staticmethod
+    # def dotw_interpreter(dotw):
+    #     if dotw in ("Monday", "monday", "Mon", "mon", "First", "first", "1"):
+    #         return 1
+    #     elif dotw in ("Tuesday", "tuesday", "Tue", "tue", "2"):
+    #         return 2
+    #     elif dotw in ("Wednesday", "wednesday", "Wed", "wed", "3"):
+    #         return 3
+    #     elif dotw in ("Thursday", "thursday", "Thu", "thu", "4"):
+    #         return 4
+    #     elif dotw in ("Friday", "firday", "Fri", "fri", "5"):
+    #         return 5
+    #     elif dotw in ("Saturday", "saturday", "Sat", "sat", "6"):
+    #         return 6
+    #     elif dotw in ("Sunday", "sunday", "Sun", "sun", "Last", "last", "7"):
+    #         return 7
+    #     else:
+    #         return 0
+
+
+def answer_interpreter(answer):
+    return True if answer in ('yes', 'yeah', 'y', 'Yes', 'Yeah', 'Y', 'Так', 'так', 'Да', 'да', 'д', 'т', 'Д', 'Т', '+',
+                              'угу', 'ага') else False
 
 
 def main():
-    pizza1 = PizzaOfTheDay('5', 50)
-    print('An order №1 is:')
-    print(pizza1)
+    if answer_interpreter(input('Do you want to try our Pizza Of The Day? (+/-): ')):
+        pizza = PizzaOfTheDay(datetime.today().weekday()+1, input('Enter a size in centimeters: '))
+    else:
+        pizza = Pizza(input('Enter a name of pizza: '), input('Enter a size in centimeters: '))
 
-    pizza2 = PizzaOfTheDay('7', 40)
-    print('An order №2 is:')
-    print(pizza2)
-
-    pizza3 = PizzaOfTheDay('tue', 35, 'mushrooms')
-    print('An order №3 is:')
-    print(pizza3)
-
-    pizza4 = PizzaOfTheDay('Wednesday', 40, 'pineapple')
-    print('An order №4 is:')
-    print(pizza4)
+    print('\n\033[92mYour order has been formed successfully!\033[0m', pizza, sep='\n')
 
 
 main()
