1136fcdf48c6f63d3404477d4dd451f32423e035
DimonStelmakh <dmytro2004yur@gmail.com>
2022-11-24 14:45:52 +0200

    Update lab3p1task1.py

--- Lab3_OOP_DmytroStelmakh/lab3p1task1.py
+++ Lab3_OOP_DmytroStelmakh/lab3p1task1.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument("values", nargs='+')
 
     args = parser.parse_args()
+    # print(getattr(operator, args.function))
 
     try:
         for ch in range(len(args.values)):
@@ -22,14 +23,11 @@ def main():
     try:
         operation = getattr(operator, args.function)
     except AttributeError:
-        print("Немає такої операції")
-        return
-
-    try:
-        operation = getattr(math, args.function)
-    except AttributeError:
-        print("Немає такої операції")
-        return
+        try:
+            operation = getattr(math, args.function)
+        except AttributeError:
+            print("Немає такої операції")
+            return
 
     try:
         print(operation(*args.values))  # розкладаємо масив на окремі параметри за допомогою астеріска
