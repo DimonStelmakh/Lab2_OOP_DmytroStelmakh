13b5bbeac88e8759da2227073da3b288a92dee6f
DimonStelmakh <dmytro2004yur@gmail.com>
2022-11-28 17:42:57 +0200

    Друга таска завершена

--- Lab3_OOP_DmytroStelmakh/lab3p1task2.py
+++ Lab3_OOP_DmytroStelmakh/lab3p1task2.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Composition:
     def __init__(self, name, quantity, price):
         self.name = name
@@ -103,11 +106,12 @@ class Composition:
             raise TypeError('Can\'t compare a "Composition" with not "Composition" or not number')
 
     def __add__(self, other):
-        if other.isdigit():
+        if isinstance(other, int) or other.isdigit():
             self.__quantity += int(other)
+            return self
 
     def __sub__(self, other):
-        if other.isdigit():
+        if isinstance(other, int) or other.isdigit():
             self.__quantity -= int(other)
 
     def __str__(self):
@@ -131,15 +135,21 @@ class Request:
 
     def __add__(self, other):
         if isinstance(other, str):
-            self.names.append(other)
+            new_self = copy.deepcopy(self)
+            new_self.names.append(other)
         elif isinstance(other, Request):
+            new_self = copy.deepcopy(self)
             for good in other:
-                self.names.append(good)
+                new_self.names.append(good)
         else:
             return NotImplemented
+        return new_self
 
     def __radd__(self, other):
         return self.__add__(other)
+
+    def __str__(self):
+        return f'{self.names}'
 
 
 def report(stock, request):
@@ -148,12 +158,33 @@ def report(stock, request):
     if not isinstance(request, Request):
         raise TypeError('The second parameter "request" should be an instance of class "Request"')
 
+    string = 'The stock is:\n'
+    string += '\n'.join(map(str, stock))
+    string += '\n\nFound goods:\n'
 
+    goods = list(map(lambda x: x.name, stock))
+    for good in request:
+        index = [i for i, stock_name in enumerate(goods) if good == stock_name]
+        if index:
+            string += f'\033[1m{good}\033[0m is \033[92mavailable\033[0m in quanity {stock[index[0]].quantity} ' \
+                      f'for price {stock[index[0]].price} per 1 unit\n'
+        else:
+            string += f'\033[1m{good}\033[0m is \033[91munavailable\033[0m\n'
+
+    return string
 
 
 def main():
-    stock = Composition()
-    print(stock)
+    stock1 = [Composition('Apple', '100', '30'), Composition('Banana', '60', '63'), Composition('Orange', '80', '55')]
+
+    request1 = Request()
+    request1 += 'Apple'
+
+    request2 = Request()
+    request2 = request1 + 'Banana'
+    request2 += 'Mandarin'
+
+    print(report(stock1, request2))
 
 
 main()
